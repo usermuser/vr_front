e5ab72fa5fc8bb8ed5d99ded23dfa8793c9622b0
usermuser <lwrdrstv@gmail.com>
2017-05-19 09:08:00 +0000

    Second commit

---
+++ vrapp/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Process',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField(max_length=50)),
+                ('start_time', models.DateTimeField()),
+                ('stop_time', models.DateTimeField()),
+                ('duration', models.DateTimeField()),
+            ],
+        ),
+    ]
